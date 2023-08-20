b196a6ba3a16999dec959402e1644dc235d68763
rita-crypto <rita.khadka552@gmail.com>
2023-08-20 16:19:14 -0400

    Added path for newproperty-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NewpropertyListComponent } from './newproperty-list/newproperty-list.component';
 
 const routes: Routes = [
-  {path: '', component: PropertyListComponent}
+  {path: '', component: PropertyListComponent},
+  {path: 'new', component: NewpropertyListComponent}
+  
 ];
 
 @NgModule({
