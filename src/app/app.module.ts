c67bb8e4604b7457a7ac25852e0f894e92ffb166
rita-crypto <rita.khadka552@gmail.com>
2023-08-20 16:20:54 -0400

    Added ng module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { PropertyListComponent } from './property-list/property-list.component';
+import { NewpropertyListComponent } from './newproperty-list/newproperty-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PropertyListComponent,
+    NewpropertyListComponent
    
   ],
   imports: [
